5db01545546ff7196edf512382b3e61e2cad396a
rofinp <rofinugraha.dev@gmail.com>
2024-10-23 21:52:17 +0700

    update main.py, add screen function

--- main.py
+++ main.py
@@ -12,9 +12,21 @@ from constants import (
 )
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    black_color = (0, 0, 0)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        # This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Exiting asteroids!")
+                pygame.quit()
+                return
+        screen.fill(black_color)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
